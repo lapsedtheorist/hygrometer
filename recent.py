39c5c28a6096af74ac135c4be7998778ef181d66
Ben Carpenter <www@bencarpenter.co.uk>
2021-03-13 22:17:44 +0000

    Dump recent data as JSON with a limit of 300 entries (5 hrs with 1/min sampling)

--- recent.py
+++ recent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timezone
+import json
 from pathlib import Path
 import sqlite3
 
@@ -15,9 +16,12 @@ def open_timeseries_database_read(path='hygrometer.db'):
     return (sql3con, sql3cur)
 
 def read_hygrometer(cursor):
-    cursor.execute('SELECT * FROM timeseries ORDER BY datetime')
+    cursor.execute('SELECT * FROM timeseries ORDER BY datetime LIMIT 300')
     return cursor.fetchall()
 
 (sql3con, sql3cur) = open_timeseries_database_read()
 data = read_hygrometer(sql3cur)
-print(data)
+keys = ("datetime", "temperature", "humidity")
+results = [dict(zip(keys, values)) for values in data]
+
+print(json.dumps(results))
