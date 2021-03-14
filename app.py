4c8cf731ca3f4408a9c230ebb875ae17c99fe136
Ben Carpenter <www@bencarpenter.co.uk>
2021-03-14 16:37:04 +0000

    Add a basic application for uWSGI usage

---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import datetime, timezone
+import json
+import pathlib
+import sqlite3
+
+parser = argparse.ArgumentParser(description='Sample the sensors of the Hygrometer')
+parser.add_argument('--db', type=pathlib.Path, default='hygrometer.db', help='Use database filepath (default: hygrometer.db)')
+args = parser.parse_args()
+
+def open_timeseries_database_read(path):
+    try:
+        db_path = path.resolve(strict=True)
+    except FileNotFoundError:
+        print('ERROR: database {} not found'.format(path))
+        exit()
+    else:
+        sql3con = sqlite3.connect(path)
+        sql3cur = sql3con.cursor()
+    return (sql3con, sql3cur)
+
+def read_hygrometer(cursor):
+    cursor.execute('SELECT * FROM timeseries ORDER BY datetime LIMIT 300')
+    return cursor.fetchall()
+
+def application(env, start_response):
+    (sql3con, sql3cur) = open_timeseries_database_read(args.db)
+    data = read_hygrometer(sql3cur)
+    keys = ("datetime", "temperature", "humidity")
+    results = [dict(zip(keys, values)) for values in data]
+    start_response('200 OK', [('Content-Type','application/json')])
+    return [str.encode(json.dumps(results))]
